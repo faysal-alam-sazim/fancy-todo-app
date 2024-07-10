180a7f66e6d2b61e03caac48b90bcfd1f79dca30
Faysal Alam <faysal.alam@sazim.io>
2024-07-10 19:18:00 +0600

    chore(FTDA): Add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import MenuBar from "./components/MenuBar/MenuBar";
 import { useDisclosure } from "@mantine/hooks";
 import CreateTask from "./components/CreateTask/CreateTask";
 import DisplayTask from "./components/DisplayTask/DisplayTask";
+import { Flex } from "@mantine/core";
 
 function App() {
   const [opened, { open, close }] = useDisclosure(false);
   return (
-    <div style={{ display: "flex", gap: "20px " }}>
-      <MenuBar open={open} />
-      <div style={{ marginTop: 20 }}>
-        <DisplayTask />
-        <CreateTask opened={opened} close={close} />
-      </div>
+    <div className="container mx-auto">
+      <Flex gap={20}>
+        <MenuBar open={open} />
+        <div style={{ marginTop: 20 }}>
+          <DisplayTask />
+          <CreateTask opened={opened} close={close} />
+        </div>
+      </Flex>
     </div>
   );
 }
