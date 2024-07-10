1c62f06c086c7c131e9fd1f5cd2f405c6c8dfad3
Faysal Alam <faysal.alam@sazim.io>
2024-07-10 19:18:00 +0600

    chore(FTDA): Add tailwindcss

--- src/components/DisplayTask/DisplayTask.tsx
+++ src/components/DisplayTask/DisplayTask.tsx
@@ -2,6 +2,7 @@ import { Flex, Card, Text, Badge, Group } from "@mantine/core";
 import { getTasksFromLocalStorage } from "../../localstorage/localstorage";
 import { useEffect, useState } from "react";
 import { Task } from "../../types/Task";
+import * as dayjs from "dayjs";
 
 function DisplayTask() {
   const [tasks, setTasks] = useState<Task[]>([]);
@@ -9,6 +10,7 @@ function DisplayTask() {
     const data = getTasksFromLocalStorage();
     setTasks(data);
   }, []);
+  console.log(tasks);
   return (
     <Flex>
       {tasks.map((task) => (
@@ -29,7 +31,10 @@ function DisplayTask() {
           </Text>
 
           <Flex>
-            <Text>Due Date:</Text>
+            <Text>
+              Due Date:
+              {task.dueDate ? dayjs(task.dueDate).toString() : "Not set"}
+            </Text>
           </Flex>
         </Card>
       ))}
