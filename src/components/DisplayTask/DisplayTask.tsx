003e0d5c9774dac2970ebd99496d447e55fe8c0e
Faysal Alam <faysal.alam@sazim.io>
2024-07-10 21:05:01 +0600

    feat(FTDA): add display task feature

--- src/components/DisplayTask/DisplayTask.tsx
+++ src/components/DisplayTask/DisplayTask.tsx
@@ -1,18 +1,25 @@
-import { Flex, Card, Text, Badge, Group } from "@mantine/core";
+import { Flex, Card, Text, Group, Button } from "@mantine/core";
 import { getTasksFromLocalStorage } from "../../localstorage/localstorage";
 import { useEffect, useState } from "react";
 import { Task } from "../../types/Task";
-import * as dayjs from "dayjs";
+import dayjs from "dayjs";
+import { IconTrash } from "@tabler/icons-react";
 
 function DisplayTask() {
   const [tasks, setTasks] = useState<Task[]>([]);
   useEffect(() => {
     const data = getTasksFromLocalStorage();
-    setTasks(data);
+    if (data.length > 0) {
+      const sortedTasks = data
+        .slice()
+        .sort((a: Task, b: Task) => Number(b.priority) - Number(a.priority));
+      return setTasks(sortedTasks);
+    }
+    return setTasks(data);
   }, []);
   console.log(tasks);
   return (
-    <Flex>
+    <Flex gap={4} wrap={"wrap"}>
       {tasks.map((task) => (
         <Card
           shadow="sm"
@@ -23,18 +30,34 @@ function DisplayTask() {
         >
           <Group justify="space-between" mt="sm" mb="xs">
             <Text fw={500}>{task.title}</Text>
-            <Badge color="pink">Edit</Badge>
+            <div className="p-2 bg-red-600 text-white rounded-full cursor-pointer">
+              <IconTrash />
+            </div>
           </Group>
 
-          <Text size="sm" c="dimmed">
+          <Text size="sm" c="dimmed" mb={4}>
             {task.description}
           </Text>
 
-          <Flex>
-            <Text>
-              Due Date:
+          <Text size="sm" c="dimmed" mb={4}>
+            Priority:{" "}
+            {task.priority === "0"
+              ? "Not set"
+              : task.priority === "1"
+              ? "Low"
+              : task.priority === "2"
+              ? "Medium"
+              : "High"}
+          </Text>
+          <Text size="sm" c="dimmed" mb={4}>
+            Due Date:
+            <span className="ml-2">
               {task.dueDate ? dayjs(task.dueDate).toString() : "Not set"}
-            </Text>
+            </span>
+          </Text>
+          <Flex justify={"space-between"} align={"center"}>
+            <Button>Edit</Button>
+            <Button color="green">Mark as complete</Button>
           </Flex>
         </Card>
       ))}
