48abdb3ca2287686b47e097a63d33e17eb526c73
Faysal Alam <faysal.alam@sazim.io>
2024-08-06 20:01:55 +0600

    feat(FTDA): Add update task api inside rtk query

--- shared/redux/rtk-apis/tasksAPI.types.ts
+++ shared/redux/rtk-apis/tasksAPI.types.ts
@@ -2,8 +2,17 @@ import { EPriority, ETaskStatus } from "@/shared/typedefs/enums";
 
 export type TCreateTaskPayload = {
   title: string;
-  description: string;
-  dueDate: Date | null;
+  description?: string;
+  dueDate?: Date | null;
   priority: EPriority;
   status: ETaskStatus;
+};
+
+export type TUpdateTaskPayload = {
+  id: string;
+  title?: string;
+  description?: string;
+  dueDate?: Date | null;
+  priority?: EPriority;
+  status?: ETaskStatus;
 };
