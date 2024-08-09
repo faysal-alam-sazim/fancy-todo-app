c34d70353967884b6ab64e0a3baa4c6ded97a065
faysal-alam-sazim <faysal.alam@sazim.io>
2024-08-09 14:54:14 +0600

    feat(FTDA): Added getAllTasks query inside rtk query api (#24)

--- shared/redux/rtk-apis/tasksAPI.ts
+++ shared/redux/rtk-apis/tasksAPI.ts
@@ -1,7 +1,15 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+import { TTask } from "@/shared/typedefs/types";
+
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3333/" }),
-  endpoints: (builder) => ({}),
+  endpoints: (builder) => ({
+    getAllTasks: builder.query<TTask[], void>({
+      query: () => "todos",
+    }),
+  }),
 });
+
+export const { useGetAllTasksQuery } = api;
